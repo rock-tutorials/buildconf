cd3947f627d5f3fb8846ba1ffb4cbdb2e64a93b5
Sascha Arnold <sascha.arnold@dfki.de>
2011-06-06 13:18:28 +0200

    Inital commit

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+set -e
+
+if ! test -f $PWD/autoproj_bootstrap; then
+    if which wget > /dev/null; then
+        DOWNLOADER=wget
+    elif which curl > /dev/null; then
+        DOWNLOADER=curl
+    else
+        echo "I can find neither curl nor wget, either install one of these or"
+        echo "download the following script yourself, and re-run this script"
+        exit 1
+    fi
+    $DOWNLOADER http://rock-robotics.org/autoproj_bootstrap
+fi
+
+ruby autoproj_bootstrap $@ git git://gitorious.org/rock-tutorials/buildconf.git branch=master
+if test "x$@" != "xlocaldev"; then
+    . $PWD/env.sh
+    autoproj update
+    autoproj fast-build
+fi
+
